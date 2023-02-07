152bc424af058a81d9444e04aacb1d0a18e6994b
Jirayu-307 <jiiirayu2546@gmail.com>
2023-02-07 17:34:17 +0700

    Web_App

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const data = ref();
+const config = {
+  method: "get",
+  url: "https://v2.jokeapi.dev/joke/Any?safe-mode",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+axios(config)
+  .then(function (response) {
+    data.value = response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+</script>
+<template>
+  <h1>JokeApi</h1>
+
+
+  <br>
+  <h1>{{ data["setup"] }}</h1>
+  <h4>{{ data.delivery }}</h4>
+</template>
+
+<style scoped></style>
